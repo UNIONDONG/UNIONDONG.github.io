583f3610b79bd7a1568669e975ae7208bc297c93
UNIONDONG <Xihangdong@163.com>
2023-11-24 07:01:31 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
+cd content
+git add .
+cd ../
 # Go To Public folder
 cd public
 # Add changes to git.
